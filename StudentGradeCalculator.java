5f4d6f6ddbc5e88ad38f4c841539254cb47bfcc8
Harshada Patil <rajendrapatil9078@gmail.com>
2026-06-06 19:40:12 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = 0;
+        double percentage;
+        char grade;
+
+        System.out.println("Enter marks of 5 subjects:");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Subject " + i + ": ");
+            total += sc.nextInt();
+        }
+
+        percentage = total / 5.0;
+
+        if (percentage >= 90) {
+            grade = 'A';
+        } else if (percentage >= 80) {
+            grade = 'B';
+        } else if (percentage >= 70) {
+            grade = 'C';
+        } else if (percentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\nTotal Marks = " + total);
+        System.out.println("Percentage = " + percentage + "%");
+        System.out.println("Grade = " + grade);
+
+        sc.close();
+    }
+}
