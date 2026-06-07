ca58d61961abc7ac6d1a1ff308073b147cdcfec1
Harshada Patil <rajendrapatil9078@gmail.com>
2026-06-07 14:14:50 +0530

    Add files via upload

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,18 +1,30 @@
 import java.util.Scanner;
 
+// CodSoft Java Internship - Task 3
+
 public class CurrencyConverter {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        double exchangeRate = 83.0; // 1 USD = 83 INR
+        System.out.println("=================================");
+        System.out.println("      CURRENCY CONVERTER");
+        System.out.println("=================================");
+        System.out.println("USD to INR Conversion");
+
+        double exchangeRate = 83.0;
 
         System.out.print("Enter amount in USD: ");
         double usd = sc.nextDouble();
 
         double inr = usd * exchangeRate;
 
-        System.out.println("Converted Amount in INR: ₹" + inr);
+        System.out.println("\n========== RESULT ==========");
+        System.out.println("USD Amount : $" + usd);
+        System.out.println("INR Amount : ₹" + inr);
+        System.out.println("============================");
+        System.out.println("Thank you for using Currency Converter!");
 
         sc.close();
     }
